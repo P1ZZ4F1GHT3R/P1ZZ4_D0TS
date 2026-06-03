d1c0dbdbfd3f7215c27eb32c8535961fcf9df068
P1ZZ4F1GHT3R <joepneurink@icloud.com>
2026-06-03 22:54:52 +0200

    test

--- hypr/.config/hypr/keybinds/applications.lua
+++ hypr/.config/hypr/keybinds/applications.lua
@@ -1,10 +1,10 @@
 -- Application keybinds
 
 hl.bind(mainMod .. " + G", hl.dsp.exec_cmd("godot"))
-hl.bind(mainMod .. " + G", hl.dsp.exec_cmd("github-desktop fullscreen 0"))
+hl.bind(mainMod .. " + G", hl.dsp.exec_cmd("github-desktop"))
 hl.bind(mainMod .. " + C", hl.dsp.exec_cmd("codium"))
 hl.bind(mainMod .. " + RETURN", hl.dsp.exec_cmd(terminal))
-hl.bind(mainMod .. " + SHIFT + RETURN", hl.dsp.exec_cmd(terminal .. " -e pipes-rs -f 45 -k heavy,light -r 0.6"))
+hl.bind(mainMod .. " + SHIFT + RETURN", hl.dsp.exec_cmd(terminal .. " -e pipes-rs -f 45 -k heavy,light -r 0.6", {fullscreen = true}))
 hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
 hl.bind(mainMod .. " + B", hl.dsp.exec_cmd(browser))
 hl.bind(mainMod .. " + A", hl.dsp.exec_cmd(launcher .. " open"))
