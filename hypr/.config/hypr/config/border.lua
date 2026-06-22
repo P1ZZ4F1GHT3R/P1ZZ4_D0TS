de630fd95ee90f6b491a9d7c16367c28ca68edae
P1ZZ4F1GHT3R <joepneurink@icloud.com>
2026-06-22 20:20:40 +0200

    Fixed an issue with autostart and waybound

--- hypr/.config/hypr/config/border.lua
+++ hypr/.config/hypr/config/border.lua
@@ -1,8 +1,8 @@
 hl.config({
     general = {
         col = {
-            active_border = { colors = { "rgb(7F75AC)", "rgb(E8232A)", "rgb(B06279)", "rgb(688BC4)", "rgb(826870)" } },
-            inactive_border = "rgba(070707ee)",
+            active_border = { colors = { "rgb(8D6B8C)", "rgb(8868CB)", "rgb(9894E5)", "rgb(D99ED3)", "rgb(5D5DA0)" } },
+            inactive_border = "rgba(03030Eee)",
         },
     },
 })
