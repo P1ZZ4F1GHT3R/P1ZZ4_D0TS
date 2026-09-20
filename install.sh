03014d96f01ad2a8b4194977e2e460c5eb125379
P1ZZ4F1GHT3R <joepneurink@icloud.com>
2026-09-20 21:34:15 +0200

    added the one font file it needed to display the icons properly lets fucking goooooo. install script seems to work and is tested on cachyos with hyprland preinstalled

--- install.sh
+++ install.sh
@@ -251,6 +251,7 @@ PACMAN_PACKAGES=(
     bash zsh thunar fastfetch yazi btop ghostty awww vscodium
     sddm python python-pip zen-browser quickshell qt6ct noto-fonts
     imagemagick hyprpm nwg-displays nwg-look brightnessctl power-profiles-daemon
+    ttf-dejavu-nerd
 )
 step "The following official packages will be installed:"
 printf '    %s\n' "${PACMAN_PACKAGES[@]}"
